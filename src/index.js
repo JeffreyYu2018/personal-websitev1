ceb30b7c4df05bfb5e09ef66eb97486dfba725fc
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-19 14:00:59 -0500

    Render contact view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import './styles';
 import './scripts';
 import IndexView from './views/IndexView';
+import AboutView from './views/AboutView';
+import ContactFormView from './views/ContactFormView';
 
-ReactDOM.render(<IndexView />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={IndexView} />
+      <Route exact path="/contact.html" component={ContactFormView} />
+      <Route exact path="/about.html" component={AboutView} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
