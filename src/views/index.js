06b21d51c1e5f2f138a94720537fc126ca0617cd
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-19 13:21:07 -0500

    appfairy: Migrate

---
+++ src/views/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+export { default as AboutView } from './AboutView'
+export { default as AllPostsView } from './AllPostsView'
+export { default as ContactView } from './ContactView'
+export { default as DetailCategoriesView } from './DetailCategoriesView'
+export { default as DetailPostsView } from './DetailPostsView'
+export { default as IconsView } from './IconsView'
+export { default as IndexView } from './IndexView'
+
+/* eslint-enable */
